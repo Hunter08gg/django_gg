08be203217e490317d2046e3219187eb148c4182
Hunter08gg <alex.mdn5000@gmail.com>
2025-06-04 20:55:35 +0300

    Добавлена модель новостей

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "created_at"]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField("Заголовок", max_length = 120)
+    desc = models.TextField("Описание, текст")
+    image = models.ImageField("Изображение", upload_to = "posts/")
+    created_at = models.DateField("Дата создания", auto_now_add = True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta: 
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        ordering = ["-created_at"]
